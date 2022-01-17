74907a10b953775ebd911624bab83a47cca630e2
Robert Brown <rbrown3@seattleu.edu>
2022-01-17 15:38:26 -0800

    Updating comments in MockDataStore.cs

--- Mine/Mine/Services/MockDataStore.cs
+++ Mine/Mine/Services/MockDataStore.cs
@@ -6,14 +6,21 @@ using Mine.Models;
 
 namespace Mine.Services
 {
+    /// <summary>
+    /// The MockDataStore class creates a framework for holding temporary data 
+    /// </summary>
     public class MockDataStore : IDataStore<ItemModel>
     {
         readonly List<ItemModel> items;
 
+        /// <summary>
+        /// The MockDataStore Constructor creates a List of ItemModel objects to use with the application
+        /// </summary>
         public MockDataStore()
         {
             items = new List<ItemModel>()
             {
+                // Creating new ItemModel objects with predefined Text, Descriptions, and Values
                 new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Equinox Gem", Description="A rare gemstone that contains healing properties.", Value=2 },
                 new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Iron Greatboots", Description="Tall boots made of Iron plate, with a moderate heel.", Value=1 },
                 new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Standard Thermal Zapper", Description="Fancy words for a heat gun or hair dryer.", Value=8 },
@@ -22,6 +29,11 @@ namespace Mine.Services
             };
         }
 
+        /// <summary>
+        /// The AddItemAsync method adds an item to the MockDataStore object
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
         public async Task<bool> AddItemAsync(ItemModel item)
         {
             items.Add(item);
@@ -29,6 +41,11 @@ namespace Mine.Services
             return await Task.FromResult(true);
         }
 
+        /// <summary>
+        /// The UpdateItem Async method changes a field in an ItemModel object stored in the MockDataStore object
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
         public async Task<bool> UpdateItemAsync(ItemModel item)
         {
             var oldItem = items.Where((ItemModel arg) => arg.Id == item.Id).FirstOrDefault();
@@ -38,6 +55,11 @@ namespace Mine.Services
             return await Task.FromResult(true);
         }
 
+        /// <summary>
+        /// The DeleteItemAsync method deletes an item from the MockDataStore object
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public async Task<bool> DeleteItemAsync(string id)
         {
             var oldItem = items.Where((ItemModel arg) => arg.Id == id).FirstOrDefault();
@@ -46,11 +68,21 @@ namespace Mine.Services
             return await Task.FromResult(true);
         }
 
+        /// <summary>
+        /// The GetItemAsync method retrieves an ItemModel from the MockDataStore object
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public async Task<ItemModel> GetItemAsync(string id)
         {
             return await Task.FromResult(items.FirstOrDefault(s => s.Id == id));
         }
 
+        /// <summary>
+        /// The GetItemAsync method retrieves an ItemModel from the MockDataStore object when there is no forceRefresh
+        /// </summary>
+        /// <param name="forceRefresh"></param>
+        /// <returns></returns>
         public async Task<IEnumerable<ItemModel>> GetItemsAsync(bool forceRefresh = false)
         {
             return await Task.FromResult(items);
