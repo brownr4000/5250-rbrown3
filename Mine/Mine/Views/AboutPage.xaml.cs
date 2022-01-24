9d594dc006f481022d70b85f4368630f36920c35
Robert Brown <rbrown3@seattleu.edu>
2022-01-23 22:37:59 -0800

    Added comments to AboutPage.xaml

--- Mine/Mine/Views/AboutPage.xaml.cs
+++ Mine/Mine/Views/AboutPage.xaml.cs
@@ -5,15 +5,20 @@ using Xamarin.Forms.Xaml;
 
 namespace Mine.Views
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
+    /// <summary>
+    /// The AboutPage class defines the code behind for the display of the AboutPage
+    /// </summary>
     [DesignTimeVisible(false)]
     public partial class AboutPage : ContentPage
     {
+        /// <summary>
+        /// The AboutPage Constructor
+        /// </summary>
         public AboutPage()
         {
             InitializeComponent();
 
+            // Defining Text of CurrentDateTimeLabel in corresponding XAML page
             CurrentDateTimeLabel.Text = System.DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss");
         }
     }
