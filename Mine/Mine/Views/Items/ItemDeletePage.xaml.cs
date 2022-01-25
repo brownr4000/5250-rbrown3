39409700751c4d22edaad66abc85d9d699b0e415
Robert Brown <rbrown3@seattleu.edu>
2022-01-24 17:45:00 -0800

    Added ItemDeletePage to project

---
+++ Mine/Mine/Views/Items/ItemDeletePage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Mine.Models;
+using Mine.ViewModels;
+
+namespace Mine.Views
+{
+    /// <summary>
+    /// The ItemReadPage class defines the code behind for the IteReadPage xaml
+    /// </summary>
+    [DesignTimeVisible(false)]
+    public partial class ItemDeletePage : ContentPage
+    {
+        ItemReadViewModel viewModel;
+
+        /// <summary>
+        /// The ItemReadPage Constructor takes an ItemDetailViewModel object and displays it on the ItemReadPage
+        /// </summary>
+        /// <param name="viewModel"></param>
+        public ItemDeletePage(ItemReadViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+
+        /// <summary>
+        /// The ItemReadPage Default Constructor
+        /// </summary>
+        public ItemDeletePage()
+        {
+            InitializeComponent();
+
+            var item = new ItemModel
+            {
+                Text = "Item 1",
+                Description = "This is an item description."
+            };
+
+            viewModel = new ItemReadViewModel(item);
+            BindingContext = viewModel;
+        }
+    }
+}
