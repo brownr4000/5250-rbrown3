a99414ff4ad1cd0add0b52941c5774dfa12b195c
Robert Brown <rbrown3@seattleu.edu>
2022-01-24 17:21:58 -0800

    Renamed Items to DataSet in ItemsIndexViewModel, ItemIndexPage.xaml, ItemIndexPage.xaml.cs

--- Mine/Mine/Views/Items/ItemIndexPage.xaml.cs
+++ Mine/Mine/Views/Items/ItemIndexPage.xaml.cs
@@ -65,7 +65,7 @@ namespace Mine.Views
         {
             base.OnAppearing();
 
-            if (viewModel.Items.Count == 0)
+            if (viewModel.DataSet.Count == 0)
                 viewModel.LoadItemsCommand.Execute(null);
         }
     }
