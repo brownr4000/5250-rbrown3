244f98d53ee3e1a435b3217bdbc4484786b758fb
Robert Brown <rbrown3@seattleu.edu>
2022-01-30 23:44:17 -0800

    Added CreateAsync to DatabaseService.cs

--- Mine/Mine/Services/DatabaseService.cs
+++ Mine/Mine/Services/DatabaseService.cs
@@ -40,9 +40,26 @@ namespace Mine.Services
             }
         }
 
-        public Task<bool> CreateAsync(ItemModel item)
+        /// <summary>
+        /// The CreateAsync method adds and inserts data into the database
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns>True if the item is added to the database</returns>
+        public async Task<bool> CreateAsync(ItemModel item)
         {
-            throw new NotImplementedException();
+            if (item == null)
+            {
+                return false;
+            }
+
+            var result = await Database.InsertAsync(item);
+
+            if (result == 0)
+            {
+                return false;
+            }
+
+            return true;
         }
 
         public Task<bool> UpdateAsync(ItemModel item)
