a697432930adab7bb1ef26da50fa9709e7dfeeda
Robert Brown <rbrown3@seattleu.edu>
2022-01-30 21:49:53 -0800

    Added DatabaseService to services in shared project, DatabaseService.cs

---
+++ Mine/Mine/Services/DatabaseService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+using SQLite;
+using Mine.Models;
+
+namespace Mine.Services
+{
+    /// <summary>
+    /// The Database Service class
+    /// </summary>
+    public class DatabaseService
+    {
+        static readonly Lazy<SQLiteAsyncConnection> lazyInitializer = new Lazy<SQLiteAsyncConnection>(() =>
+        {
+            return new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
+        });
+
+        // Variables to initialize database
+        static SQLiteAsyncConnection Database => lazyInitializer.Value;
+        static bool initialized = false;
+
+        public DatabaseService()
+        {
+            InitalizeAsync().SafeFireAndForget(false);
+        }
+
+        async Task InitializeAsync()
+        {
+            if (!initialized)
+            {
+                if (!Database.TableMappings.Any(m => m.MappedType.Name == typeof(ItemModel).Name))
+                {
+                    await Database.CreateTablesAsync(CreateFlags.None, typeof(ItemModel)).ConfigureAwait(false);
+                }
+
+                initialized = true;
+            }
+        }
+    }
+}
