b2e9300cd7e92182bd56b804c772d3dd957928b1
Robert Brown <rbrown3@seattleu.edu>
2022-02-07 23:49:08 -0800

    Added RollDice_Valid_Roll_1_Dice_6 to DiceHelperUnitTest.cs

--- UnitTests/Helpers/DiceHelperUnitTests.cs
+++ UnitTests/Helpers/DiceHelperUnitTests.cs
@@ -10,6 +10,9 @@ namespace UnitTests.Helpers
     [TestFixture]
     public class DiceHelperUnitTests
     {
+        /// <summary>
+        /// Test to check that a die roll zero times is zero
+        /// </summary>
         [Test]
         public void RollDice_Invalid_Roll_Zero_Should_Return_Zero()
         {
@@ -22,6 +25,24 @@ namespace UnitTests.Helpers
 
             // Assert
             Assert.AreEqual(0, result);
+        }
+
+        /// <summary>
+        /// Test to check that a six-sided die returns a result between 1 and 6
+        /// </summary>
+        [Test]
+        public void RollDice_Valid_Roll_1_Dice_6_Should_Return_Between_1_And_6()
+        {
+            // Arrange
+
+            // Act
+            var result = DiceHelper.RollDice(1, 6);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(true, result >= 1);
+            Assert.AreEqual(true, result <= 6);
         }
     }
 }
