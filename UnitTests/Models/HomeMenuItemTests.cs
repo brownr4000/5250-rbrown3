f6e9841d1ab4b8e0d79e2238069cedf7e8c30713
Robert Brown <rbrown3@seattleu.edu>
2022-02-07 16:09:51 -0800

    Added comments to HomeMenuItemTests.cs

--- UnitTests/Models/HomeMenuItemTests.cs
+++ UnitTests/Models/HomeMenuItemTests.cs
@@ -3,9 +3,15 @@ using Mine.Models;
 
 namespace UnitTests.Models
 {
+    /// <summary>
+    /// The HomeMenuItemTests class is the structure for Unit Tests of the HomeMenuItem class
+    /// </summary>
     [TestFixture]
     public class HomeMenuItemTests
     {
+        /// <summary>
+        /// Tests the default HomeMenuItem Constructor
+        /// </summary>
         [Test]
         public void HomeMenuItem_Constructor_Valid_Default_Should_Pass()
         {
@@ -20,6 +26,9 @@ namespace UnitTests.Models
             Assert.IsNotNull(result);
         }
 
+        /// <summary>
+        /// Tests the get and set of the attributes of the HomeMenuItem object
+        /// </summary>
         [Test]
         public void HomeMenuItem_Set_Get_Valid_Default_Should_Pass()
         {
